b71f4d693624f6dd2bca6770cb34cb98094655d1
Илья Третьяков <eliastrt@gmail.com>
2022-10-07 17:48:57 +0300

    HW 3 . v1.3 рефоктор по замечаниям ревью

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -4,3 +4,4 @@ from django.test import TestCase
 class Test(TestCase):
     def test_example(self):
         pass
+    
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -101,6 +101,6 @@ def post_edit(request, post_id):
     context = {
         'form': form,
         'is_edit': is_edit,
-        }
+    }
 
     return render(request, 'posts/create_post.html', context)
